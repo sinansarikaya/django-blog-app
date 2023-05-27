f8b881b3c8f0527d608a1e5fd8e99e8a020342ae
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-27 08:58:50 +0200

    Core

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Post(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+def post_list(request):
+    pass
+
+def post_detail(request, slug):
+    pass
+
+@login_required
+def post_create(request):
+    pass
+
+@login_required
+def post_edit(request, slug):
+    pass
+
+
+@login_required
+def add_comment(request, slug):
+    pass
+
+@login_required
+def comment_approve(request, pk):
+    pass
+
+
