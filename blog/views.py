26496fd67f31840df4100fed7061db7fba7543ee
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-27 18:14:48 +0200

    CREATE: Loing and Register pages

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'blog/login.html')
+
+def register_view(request):
+    return render(request, 'blog/register.html')
+
+def logout_view(request):
+    return redirect('/')
+
+
