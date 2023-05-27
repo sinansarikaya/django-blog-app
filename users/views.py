169712361813f604d5b907fec8d713c9160423c0
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-28 01:51:11 +0200

    CREATE: Login view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from blog.models import Post
 
 def dashboard(request):
@@ -9,12 +11,21 @@ def profile(request):
     return render(request, 'users/profile.html')
 
 def login_view(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
 
 def register_view(request):
     return render(request, 'users/register.html')
 
 def logout_view(request):
+    logout(request)
     return redirect('/')
 
 
