dcab2fe582269addb58bd9e35db26641669a37de
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-27 18:38:53 +0200

    CREATE: Profile and dashboard pages

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('dashboard/', dashboard, name='dashboard'),
+    path('profile/', profile, name='profile'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'), 
     path('logout/', logout_view, name='logout'), 
--- users/views.py
+++ users/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+def dashboard(request):
+    posts = [
+        {
+            "id": 1,
+            "title": "Örnek Blog Başlığı 1",
+            "date": "2023-05-10"
+        },
+        {
+            "id": 2,
+            "title": "Örnek Blog Başlığı 2",
+            "date": "2023-05-15"
+        },
+        {
+            "id": 3,
+            "title": "Örnek Blog Başlığı 3",
+            "date": "2023-05-20"
+        }
+    ]
+    return render(request, 'users/dashboard.html', {'posts':posts})
+
+def profile(request):
+    return render(request, 'users/profile.html')
+
 def login_view(request):
-    return render(request, 'blog/login.html')
+    return render(request, 'users/login.html')
 
 def register_view(request):
-    return render(request, 'blog/register.html')
+    return render(request, 'users/register.html')
 
 def logout_view(request):
     return redirect('/')
