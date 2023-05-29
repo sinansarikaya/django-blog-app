41ddfadae028205f375c68d71f66e09de95dbdaf
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-29 13:23:26 +0200

    UPDATE: build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# Create a virtual environment and activate it
+python -m venv venv
+source venv/bin/activate
+
+# Install required packages and run collectstatic
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
